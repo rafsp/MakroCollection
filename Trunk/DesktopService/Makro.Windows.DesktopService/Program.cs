b8cb0150fd141099502cf91fd7c2987a91052919
Edimar Lima de Souza <t_elsouza@MAKRO.COM.BR>
2015-03-10 13:29:43 +0000

    Initial check-in
    git-tfs-id: [http://brsptfs001:8080/tfs/MakroCollection]$/Makro.Windows;C29440

---
+++ Trunk/DesktopService/Makro.Windows.DesktopService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Makro.Windows.DesktopService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new DesktopService() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
